7f4d450ab7a68c6712b6a9e39176f6069c8ced64
octopuscart <dev@develop.com>
2020-12-30 18:46:28 +0530

    changes

---
+++ application/config/configuration_db.php
@@ -0,0 +1,5 @@
+<?php
+$activedb = "j2k5e6r5_woodcoupon";
+$activedbusername = "j2k5e6r5_octopus";
+$activedbpassword = "India$2017";
+?>
---
+++ application/libraries/Phpqr.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+include_once APPPATH . '/third_party/phpqrcode/qrlib.php';
+
+class Phpqr {
+
+
+    public function __construct() {}
+    public function createcode($text, $file){
+        $this->$text = $text;
+        $this->$file = $file;
+        QRcode::png($text, $file, "L", "10", "10");
+    }
+
+}
+
+?>
